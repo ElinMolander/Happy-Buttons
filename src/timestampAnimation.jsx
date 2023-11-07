b85282279c6db20796d293d6a4c5e233a6278628
ElinMolander <105853122+ElinMolander@users.noreply.github.com>
2023-11-07 20:24:23 +0100

    Separation of concerns

---
+++ src/timestampAnimation.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+const newAnimation = [
+    {
+      start: 0,
+      end: 10,
+      loop: false,
+    },
+    {
+     loop: false, 
+     start: 0,
+     end: 188, 
+   },
+   {
+     loop: false, 
+     start: 205,
+     end: 351, 
+   },
+   {
+     loop: false, 
+     start: 389,
+     end: 469, 
+   },
+   {
+     loop: false, 
+     start: 488,
+     end: 509, 
+   },
+   {
+     loop: false, 
+     start: 530,
+     end: 787, 
+   },
+   {
+     loop: false, 
+     start: 878,
+     end: 1189, 
+   },
+   {
+     loop: true, 
+     start: 1219,
+     end: 1264, 
+   },
+   {
+     loop: false, 
+     start: 1262,
+     end: 1418, 
+   },
+   {
+     loop: false, 
+     start: 1414,
+     end: 1486, 
+   },
+  ]
+
+
+  export default newAnimation
